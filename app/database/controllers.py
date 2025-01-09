73fc6bb8b2324e6d18012fa2f8290271860152d3
mimij146 <mimij@live.co.uk>
2025-01-09 17:05:41 +0000

    Add Distinct Areas

--- app/database/controllers.py
+++ app/database/controllers.py
@@ -45,6 +45,11 @@ class Database:
     def get_avg_act(self):
         """Returns the average ACT cost"""      
         return round(db.session.execute(db.select(func.avg(PrescribingData.ACT_cost))).first()[0],2)
+    
+    def get_distinct_areas(self):
+        """Returns the total number of areas."""
+        result = db.session.execute(db.select(PracticeData.area).distinct()).all()
+        return len(tuple(self.convert_tuple_list_to_raw(result)))
 
 
 db.session.execute
